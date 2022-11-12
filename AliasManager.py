65c493bd29207a98ca9b1c1d0c73cb7f9590f525
Daniel <daniel@solcus.com>
2022-11-12 23:26:04 +0100

    Added inline documentation

--- AliasManager.py
+++ AliasManager.py
@@ -8,12 +8,23 @@ class AliasManager:
 
     def __init__(self, filepath: str = "") -> None:
         """
+        Initialization of the AliasManager. 
+        Loads data from filepath and creates a backup
+
+        param filepath: filepath of the aliases. Optional.
+
+        returns None
         """
         self.filedata = self.__load_file(filepath)
         self.__create_backup()     
 
     def __load_file(self, filepath: str) -> str:
         """
+        Loads file from filepath
+
+        param filepath: Filepath of the aliasses. (Optional: Will otherwise take system default)
+
+        returns str: File data from the loaded file
         """
         try:
             self.filepath = filepath
@@ -24,8 +35,13 @@ class AliasManager:
             with open(self.filepath, "r") as file:
                 return file.read()
             
-    def __save_file(self, suffix: str = ""):
+    def __save_file(self, suffix: str = "") -> None:
         """
+        Saves file to the set filepath.
+
+        param suffix: Add a suffix to the savefile.
+
+        returns None
         """
         try:
             with open(f"{self.filepath}{suffix}", "w") as outfile:
@@ -33,21 +49,37 @@ class AliasManager:
         except FileNotFoundError:
             exit(f"Couldn't write to file.")
 
-    def __create_backup(self):
+    def __create_backup(self) -> None:
         """
+        Creates a backup of the filedata
+
+        returns None
         """
         self.__save_file(".prev")
 
-    def add(self, key: str, value: str):
+    # PUBLIC METHODS
+
+    def add(self, key: str, value: str) -> None:
         """
+        Add an alias to the system aliases
+        
+        param key: Key for the alias
+        param value: Command for the alias
+
+        returns None
         """
         newline = key+'="' + value + '"'
         self.filedata = f"{self.filedata}\nalias {newline}"
         print(f"Adding line  : '{newline}'")
         self.__save_file()
 
-    def remove(self, key: str):
+    def remove(self, key: str) -> None:
         """
+        Remove an alias from the system aliases
+        
+        param key: Key of the alias
+
+        returns None
         """
         lines = self.filedata.split("\n")
         search_for = f"alias {key}="
@@ -61,11 +93,20 @@ class AliasManager:
         self.filedata = newdata
         self.__save_file()
         
-    def show(self, key: str):
+    def show(self, key: str = "") -> None:
         """
+        Show an alias of the system aliases
+        
+        param key: Key of the alias
+
+        returns None
         """
         lines = self.filedata.split("\n")
-        search_for = f"alias {key}="
+        if key:
+            search_for = f"alias {key}="
+        else:
+            search_for = "alias "
+            
         displayline = ""
         for line in lines:
             if line.find(search_for) >= 0:
